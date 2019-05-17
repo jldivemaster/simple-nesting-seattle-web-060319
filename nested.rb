1c538fb93c71584bf22d3f0d3c080c7885bad82d
Jason Leach <jlspursfan@gmail.com>
2019-05-17 20:18:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+  return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+  return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -80,7 +84,11 @@ def adding_matz
         }
      }
 
-
+  programmer_hash[:yukihiro_matsumoto] = {
+	   :known_for => "Ruby",
+	    :languages => ["LISP", "C"]
+	 }
+  return programmer_hash
 end
 
 def changing_alan
@@ -101,8 +109,10 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
+  
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
 
-
+	return programmer_hash
 end
 
 def adding_to_dennis
